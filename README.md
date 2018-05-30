cb4a0070901d4acab717b0a7ea775aafa04883f5
Christian Schrötter <cs@fnx.li>
2018-05-30 22:33:19 +0200

    Release 0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# acme-dns-nc
-Use netcup CCP-DNS-API for ACME's dns-01 challenge
+# ACME-DNS-NC
+Simple helper script for various [Let's Encrypt][1] clients.
+Developed for [GetSSL][2], tested at Debian and Ubuntu.
+
+**NO WILDCARD SUPPORT VIA ACMEv2!** (see issue [#1][1001])
+
+## Initial setup
+Download or clone the archive and extract it to a new folder.
+
+Copy the example config file `config/.nc-ccp.ini` to `~/.nc-ccp.ini` and insert
+your API credentials. Don't forget to check file permissions! (recommended: 0600)
+
+Run it for the first time:
+
+```bash
+# Add the TXT record _acme-challenge.example.com with value "test":
+./scripts/acme-dns-nc "example.com" "test"
+
+# Check your nameserver: (wait some time)
+dig TXT "_acme-challenge.example.com" +short
+
+# Delete the TXT record _acme-challenge.example.com:
+./scripts/acme-dns-nc --del "example.com"
+```
+
+Take a look at the wiki for more examples.
+
+## Bugs? Feedback?
+Open a new issue or drop me a line at cs@fnx.li! :-)
+
+Important: This project is **not** affiliated with netcup GmbH!
+
+## Important links...
+* [Bugtracker](https://github.com/froonix/acme-dns-nc/issues)
+* [Wiki pages](https://github.com/froonix/acme-dns-nc/wiki)
+* [netcup Domains (DE)](https://www.netcup.de/bestellen/domainangebote.php?ref=13994)
+* [netcup Domains (EN)](https://www.netcup.eu/bestellen/domainangebote.php?ref=13994)
+* [netcup CCP-DNS-API](https://ccp.netcup.net/run/webservice/servers/endpoint.php)
+* [Let's Encrypt](https://letsencrypt.org/)
+
+[1]: https://letsencrypt.org/docs/client-options/
+[2]: https://github.com/srvrco/getssl
+
+[1001]: https://github.com/froonix/acme-dns-nc/issues/1
---
+++ tests/simple-test.sh
@@ -0,0 +1,124 @@
+#!/usr/bin/env bash
+###############################################
+#                                             #
+# First simple test script for ACME-DNS-NC.   #
+# Try to add and delete 100 random records.   #
+#                                             #
+# Example: simple-test.sh example.org \       #
+#                 root-dns.netcup.net         #
+#                                             #
+###############################################
+
+set -euf -o pipefail
+cd "$(dirname "$(readlink -f "$0")")"
+
+DOMAIN=${1-}; NS=${2-DEFAULT}
+SCRIPT=${3-../scripts/acme-dns-nc}
+WAIT_TRIES=100; WAIT_DNS=12; WAIT_NEXT=5
+CHALLENGE_PREFIX="_acme-challenge"
+
+if [[ "$DOMAIN" == "" ]]; then echo "Usage: $0 <domain> [<primary-nameserver> [<path-to-script>]]" 1>&2; exit 1
+elif [[ ! -x "$SCRIPT" ]]; then echo "Invalid path to script: $SCRIPT" 1>&2; exit 1; fi
+if [[ "$NS" == "DEFAULT" ]]; then NS=; else NS="@$NS"; fi
+
+tmpfile=`mktemp`; logfile=`mktemp`
+echo "Logfile for errors: ${logfile}"
+
+function status
+{
+	pre=${1-UNKNOWN}; msg=${2-}; ext=${3-}
+	if [[ "$ext" != "" ]]; then ext=": $ext"; fi
+	echo -ne "\r$(date +%H:%M:%S) [$pre] $msg$ext"
+}
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+RESET='\033[0m'
+
+for i in {1..100}
+do
+	# \n
+	echo
+
+	# Get random values for hostname and TXT record data.
+	hostname="test-$(printf '%05d' "$RANDOM")-$(printf '%05d' "$RANDOM").$DOMAIN"
+	txtvalue="test-$RANDOM-$RANDOM-$RANDOM-$(date +%Y%m%d-%H%M%S)"
+
+	# Try to add the DNS record.
+	status "${YELLOW}ADD RR${RESET}" "$hostname"
+	return=0; "$SCRIPT" "$hostname" "$txtvalue" 1>/dev/null 2>>"$logfile" || return=$? && :
+
+	if [[ "$return" != "0" ]]
+	then
+		status "${RED}ADD RR${RESET}" "$hostname" "Failed!  (could not create or update DNS-RR)"
+		continue
+	fi
+
+	final=-1
+	for((w=1; w<=WAIT_TRIES; w++))
+	do
+		# Wait for the primary NS to update its zone...
+		status "${YELLOW}WAIT#1${RESET}" "$hostname" "($w/$WAIT_TRIES)                             "
+		sleep "$WAIT_DNS"
+
+		# Check the DNS record.
+		status "${YELLOW}CHK #1${RESET}" "$hostname"
+		return=0; dig +short "TXT" "$CHALLENGE_PREFIX.$hostname" "$NS" 1>"$tmpfile" 2>/dev/null || return=$? && :
+
+		if [[ "$return" == "0" && "$(cat "$tmpfile")" == "\"$txtvalue\"" ]]
+		then final=0; break; else final=1; fi
+	done
+
+	if [[ "$final" != "0" ]]
+	then
+		status "${RED}CHK #1${RESET}" "$hostname" "Failed!  (value not found in DNS)"
+		continue
+	fi
+
+	# Try to delete the DNS record.
+	status "${YELLOW}DEL RR${RESET}" "$hostname"
+	return=0; "$SCRIPT" "$hostname" 1>/dev/null 2>>"$logfile" || return=$? && :
+
+	if [[ "$return" != "0" ]]
+	then
+		status "${RED}DEL RR${RESET}" "$hostname" "Failed!  (could not delete DNS-RR)"
+		continue
+	fi
+
+	final=-1
+	for((w=1; w<=WAIT_TRIES; w++))
+	do
+		# Wait for the DNS cache to expire...
+		status "${YELLOW}WAIT#2${RESET}" "$hostname" "($w/$WAIT_TRIES)                             "
+		sleep "$WAIT_DNS"
+
+		# Check the DNS record.
+		status "${YELLOW}CHK #2${RESET}" "$hostname"
+		return=0; dig +short "TXT" "$CHALLENGE_PREFIX.$hostname" "${NS}" 1>"$tmpfile" 2>/dev/null || return=$? && :
+
+		if [[ "$return" == "0" && "$(cat "$tmpfile")" == "" ]]
+		then final=0; break; else final=1; fi
+	done
+
+	if [[ "$final" != "0" ]]
+	then
+		status "${RED}CHK #2${RESET}" "$hostname" "Failed!  (record still exists)"
+		continue
+	fi
+
+	# Wait for the next test run...
+	status "${GREEN}  OK  ${RESET}" "$hostname" "Success!                          "
+	sleep "$WAIT_NEXT"
+
+done
+
+if [[ "0$(stat --printf="%s" "$logfile")" == "00" ]]
+then
+	echo -e "\n\nDeleting empty logfile: $logfile"
+	rm -f "$logfile"
+else
+	echo -e "\n\nLogfile is not empty: $logfile"
+fi
+
+rm -f "$tmpfile"
